84be7487695ec4ad12277e18c8cd2efd23108bd0
Daniel Groß <daniel@grossmail.io>
2026-04-09 17:51:53 +0200

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,109 @@
+import random
+
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+word_list = ["aardvark", "baboon", "camel", "apple", "automobile", "advisory", "checkpoint" ]
+
+lives = 6
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+placeholder = ""
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
+
+game_over = False
+correct_letters = []
+guessed_letters = []
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in guessed_letters:
+        print("You already guessed that letter.")
+        continue
+
+    guessed_letters.append(guess)
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print("You lose!")
+            print(f"The word was: {chosen_word}")
+
+    print(stages[lives])
+
+    if "_" not in display and not game_over:
+        game_over = True
+        print("You win!")
